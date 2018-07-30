dd686fcff5050f754c0f60fe0eadaafec72729df
Minh Anh Le <lethiminhanh@secure.janeto.com>
2018-07-30 17:05:05 +0700

    update init project

---
+++ src/components/Login.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="login">
+        <div>
+            <h3>Login</h3>
+            <form>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div>
+                    <label><input type="checkbox"> Remember me</label>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-default">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {}
+  },
+  methods: { }
+}
+</script>
+<style>
+    .login {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: inherit;
+    }
+
+    .login form {
+        width: 600px;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import BootstrapVue from "bootstrap-vue"
-import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
+import App from './App'
+import router from './router'
+import JQuery from 'jquery'
+import 'bootstrap/dist/css/bootstrap.css'
+global.JQuery = JQuery
+global.$ = JQuery
 
-Vue.use(BootstrapVue)
+Vue.config.productionTip = false
 
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  components: { App },
+  template: '<App/>'
 })
